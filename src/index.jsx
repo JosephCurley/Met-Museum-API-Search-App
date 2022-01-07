ca796952357459da305edc3e585946f7ab3f5edd
Stephen Marsh <stephenhmarsh@users.noreply.github.com>
2022-01-07 14:41:01 -0500

    get tesseract to ocr example png in the browser

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createWorker } from 'tesseract.js';
 import App from './App.jsx';
+
+import objectLabel from '../example/object_label_cropped.png';
+
+const worker = createWorker({
+	logger: m => console.log(m),
+});
+
+(async () => {
+	await worker.load();
+	await worker.loadLanguage('eng');
+	await worker.initialize('eng');
+	const {
+		data: { text },
+	} = await worker.recognize(objectLabel);
+	console.log(text);
+	await worker.terminate();
+})();
 
 ReactDOM.render(<App />, document.getElementById('app'));
