1b7c15b889db8daebbcbf6c458d5807539730ca2
Joseph Curley <hey.josephcurley@gmail.com>
2022-01-10 09:54:41 -0500

    Update Active Object Display

--- src/components/active-object/index.jsx
+++ src/components/active-object/index.jsx
@@ -5,30 +5,39 @@ const ActiveObject = ({ object, handleSavedObjectChange, savedObjects }) => (
 	<div className="active-object">
 		<div>
 			<div className="active-object__title-box">
-				<div>
+				<div className="active-object__titles">
 					<h1
 						className="active-object__header"
 						dangerouslySetInnerHTML={{ __html: object.title }}
 					/>
 					<h2 className="active-object__artist">{object.artistDisplayName}</h2>
 				</div>
-				{savedObjects[object.objectID] ? (
-					<button
-						onClick={handleSavedObjectChange}
-						onKeyDown={e => e.key === 'Enter' && handleSavedObjectChange}
-						className="active-object__button active-object__button--remove"
-						type="submit">
-						Remove
-					</button>
-				) : (
-					<button
-						onClick={handleSavedObjectChange}
-						onKeyDown={e => e.key === 'Enter' && handleSavedObjectChange}
-						className="active-object__button active-object__button--save"
-						type="submit">
-						Save 🤍
-					</button>
-				)}
+				<div className="active-object__buttons">
+					{savedObjects[object.objectID] ? (
+						<button
+							onClick={handleSavedObjectChange}
+							onKeyDown={e => e.key === 'Enter' && handleSavedObjectChange}
+							className="active-object__button active-object__button--remove button button--secondary"
+							type="submit">
+							Remove
+						</button>
+					) : (
+						<button
+							onClick={handleSavedObjectChange}
+							onKeyDown={e => e.key === 'Enter' && handleSavedObjectChange}
+							className="active-object__button active-object__button--save button button--primary"
+							type="submit">
+							Save 🤍
+						</button>
+					)}
+					<a
+						href={object.objectURL}
+						target="_blank"
+						rel="noreferrer"
+						className="button button--tertiary">
+						View Object Page
+					</a>
+				</div>
 			</div>
 			{object.primaryImageSmall && (
 				<img
@@ -45,7 +54,14 @@ const ActiveObject = ({ object, handleSavedObjectChange, savedObjects }) => (
 						dangerouslySetInnerHTML={{ __html: object.objectName }}
 					/>
 				</div>
-
+				{object.artistDisplayName && (
+					<div className="active-object__info">
+						<span className="active-object__key">{object.artistRole}: </span>
+						<span className="active-object__value">
+							{object.artistDisplayName}
+						</span>
+					</div>
+				)}
 				<div className="active-object__info">
 					<span className="active-object__key">Accession Number: </span>
 					<span className="active-object__value">{object.accessionNumber}</span>
@@ -59,18 +75,6 @@ const ActiveObject = ({ object, handleSavedObjectChange, savedObjects }) => (
 				<div className="active-object__info">
 					<span className="active-object__key">Department: </span>
 					<span className="active-object__value">{object.department}</span>
-				</div>
-
-				<div className="active-object__info">
-					<span className="active-object__value">
-						<a
-							href={object.objectURL}
-							target="_blank"
-							rel="noreferrer"
-							className="active-object__link">
-							View Object Page
-						</a>
-					</span>
 				</div>
 			</div>
 		</div>
