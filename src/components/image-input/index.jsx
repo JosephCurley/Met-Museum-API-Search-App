70e752910a6941c6f19a2c8744a699edba430a4c
Joseph Curley <hey.josephcurley@gmail.com>
2022-01-12 10:33:22 -0500

    Add image resizer, probably a dead end

--- src/components/image-input/index.jsx
+++ src/components/image-input/index.jsx
@@ -21,7 +21,6 @@ const ImageInput = ({ searchObjects }) => {
 				const { data } = await worker.recognize(reader.result);
 				await worker.terminate();
 				data.lines.forEach(line => {
-					alert(line.text);
 					const firstChunkOfText = line.text.split(' ')[0].replace(/\n/g, '');
 					if (firstChunkOfText.match(accessionRegex)) {
 						searchObjects(firstChunkOfText);
@@ -37,10 +36,70 @@ const ImageInput = ({ searchObjects }) => {
 		};
 	};
 
+	const dataURLToBlob = dataURL => {
+		const BASE64_MARKER = ';base64,';
+		if (dataURL.indexOf(BASE64_MARKER) === -1) {
+			const parts = dataURL.split(',');
+			const contentType = parts[0].split(':')[1];
+			const raw = parts[1];
+
+			return new Blob([raw], { type: contentType });
+		}
+
+		const parts = dataURL.split(BASE64_MARKER);
+		const contentType = parts[0].split(':')[1];
+		const raw = window.atob(parts[1]);
+		const rawLength = raw.length;
+
+		const uInt8Array = new Uint8Array(rawLength);
+
+		for (let i = 0; i < rawLength; i += 1) {
+			uInt8Array[i] = raw.charCodeAt(i);
+		}
+
+		return new Blob([uInt8Array], { type: contentType });
+	};
+
+	const resizeImage = file => {
+		const reader = new FileReader();
+		reader.onload = readerEvent => {
+			const image = new Image();
+			image.onload = () => {
+				// Resize the image
+				const canvas = document.createElement('canvas');
+				const maxSize = 544;
+				let { width, height } = image;
+				if (width > height) {
+					if (width > maxSize) {
+						height *= maxSize / width;
+						width = maxSize;
+					}
+				} else if (height > maxSize) {
+					width *= maxSize / height;
+					height = maxSize;
+				}
+				canvas.width = width;
+				canvas.height = height;
+				canvas.getContext('2d').drawImage(image, 0, 0, width, height);
+				const dataUrl = canvas.toDataURL('image/jpeg');
+				const resizedImage = dataURLToBlob(dataUrl);
+				console.log(resizedImage);
+				// $.event.trigger({
+				// 	type: 'imageResized',
+				// 	blob: resizedImage,
+				// 	url: dataUrl,
+				// });
+			};
+			image.src = readerEvent.target.result;
+		};
+		readImage(file);
+	};
+
 	const handleOnChange = file => {
-		alert(file.type);
 		if (file && file.type.includes('image')) {
-			readImage(file);
+			console.log(file);
+			resizeImage(file);
+			//	readImage(file);
 		}
 	};
 
